2c11dd98e25a1de384b24492f10b417c9245741e
Daniel Mendoza Pupo <daniel.mendozapupo@charter.com>
2021-10-09 17:08:25 +0000

    Better structure: added a demo folder and put the project on it that way I can call the project from outside providing the project folder as a private data dir

--- runner_ansible.py
+++ runner_ansible.py
@@ -1,11 +1,14 @@
 import ansible_runner
 
 if __name__ == '__main__':
-    runner = ansible_runner.run(private_data_dir='/home/dpupo/ansible-runner/inventory', playbook='/home/dpupo/ansible-runner/project/test.yml')
-    print("{}: {}".format(runner.status, runner.rc))
+    runner = ansible_runner.run(private_data_dir='/home/dpupo/ansible-runner/demo/',
+                                playbook='/home/dpupo/ansible-runner/demo/project/test.yml')
+
+    print("Runner Status:\n {}: {}".format(runner.status, runner.rc))
 
     # Successful: 0
     for each_host_event in runner.events:
         print(each_host_event['event'])
     print("Final status: ")
     print(runner.stats)
+    
